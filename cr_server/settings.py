e2e6a1b6dce038182ec0e4150fc42f4a53cc37de
jordanbradley <jbradley@mica.edu>
2015-04-04 22:00:40 -0400

    lots of progress on the animations

--- cr_server/settings.py
+++ cr_server/settings.py
@@ -113,6 +113,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'cr_www'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- cr_www/static/js/index.js
+++ cr_www/static/js/index.js
@@ -1,1 +1,21 @@
 //example js
+$(document).ready(function(){
+
+    //tab system
+    var activeTab = '';
+    $(".feature-tab").each(function(i){
+       $(this).click(function(){
+           activeTab = $(this).attr("tab-id");
+           $(this).addClass("active");
+           $(".feature-tab").each(function(i){
+              if(activeTab != $(this).attr("tab-id")){
+                  $(this).removeClass("active");
+              }
+           });
+       });
+    });
+
+    window.setTimeout(function(){
+        $("#overview-tab").click();
+    }, 1000);
+});
